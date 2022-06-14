91825c1bd7b2461973871945ffe3bc966e623bf8
yeppeo <atmai2002@gmail.com>
2022-06-15 02:55:01 +1000

    Halfway through user stats page

--- src/pages/tournament_view/user_stats/user_stats.jsx
+++ src/pages/tournament_view/user_stats/user_stats.jsx
@@ -1,9 +1,33 @@
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 function UserStats() {
-    return (
-        <h2>Your Stats</h2>
 
+    const instruments = [
+        {
+            "name": "Instrument 1",
+            "position": "",
+            "profit": "",
+            "recentTrades": []
+        },
+        {
+            "name": "Instrument 2",
+            "position": "",
+            "profit": "",
+            "recentTrades": []
+        }
+    ]
+
+    return ( // TODO: Handle onClick when dropdown item is clicked
+        <div>
+            <h2>Your Stats</h2>
+            <DropdownButton id="dropdown-basic-button" title="Instruments">
+                {instruments.map(
+                    (instrument) => (
+                        <Dropdown.Item>{instrument.name}</Dropdown.Item>
+                    )
+                )}
+            </DropdownButton>
+        </div>
     )
 }
 
