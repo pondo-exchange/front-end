ee4ac9c6839d87ef791949439c9aec5e11a22f39
yeppeo <atmai2002@gmail.com>
2022-06-16 21:33:38 +1000

    Added previous trades to user stats section

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Available routes:\
 /\
 /instrument-view\
 /profile\
+/register\
 /tournament-view\
 /tournaments-list\
--- src/pages/tournament_view/user_stats/user_stats.jsx
+++ src/pages/tournament_view/user_stats/user_stats.jsx
@@ -1,13 +1,13 @@
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import { React, useState } from "react";
+import { Dropdown, DropdownButton, Container, Row, Col } from "react-bootstrap";
 
 function UserStats() {
-
     const instruments = [
         {
             "name": "Instrument 1",
             "position": "",
             "profit": "",
-            "recentTrades": []
+            "recentTrades": ["BUY", "SELL"]
         },
         {
             "name": "Instrument 2",
@@ -17,17 +17,46 @@ function UserStats() {
         }
     ]
 
+    const [currInstrument, setCurrInstrument] = useState(instruments[0]);
+
     return ( // TODO: Handle onClick when dropdown item is clicked
         <div>
             <h2>Your Stats</h2>
-            <DropdownButton id="dropdown-basic-button" title="Instruments">
+            <DropdownButton id="dropdown-basic-button" title={currInstrument.name}>
                 {instruments.map(
-                    (instrument) => (
-                        <Dropdown.Item>{instrument.name}</Dropdown.Item>
-                    )
+                    (instrument) => (<Dropdown.Item onClick={() => setCurrInstrument(instrument)}>{instrument.name}</Dropdown.Item>)
                 )}
             </DropdownButton>
+            <InstrumentInfo currInstrument={currInstrument} />
         </div>
+    )
+}
+
+function InstrumentInfo(props) {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    {props.currInstrument.position}
+                </Col>
+                <Col>
+                    {props.currInstrument.profit}
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    {props.currInstrument.recentTrades.map(
+                        (trade) => (<Trade trade={trade} />)
+                    )}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+function Trade(props) {
+    return (
+        <h2>{props.trade}</h2>
     )
 }
 
