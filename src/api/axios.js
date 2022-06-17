7b606bd8343250e89fe48508e3630cf5bdf17b1d
yeppeo <atmai2002@gmail.com>
2022-06-17 22:50:22 +1000

    Adding package lock back

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "https://cors-anywhere.herokuapp.com/http://projectpondo.com:8080/api"  // To be used when needing to bypass CORS
-    baseURL: "http://projectpondo.com:8080/api"
+    baseURL: "https://cors-anywhere.herokuapp.com/http://projectpondo.com:8080/api"  // To be used when needing to bypass CORS
+    // baseURL: "http://projectpondo.com:8080/api"
 });
