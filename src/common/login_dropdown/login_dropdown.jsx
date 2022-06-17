fc452ced56ca61e924680c6afc8db1f9594746b5
yeppeo <atmai2002@gmail.com>
2022-06-17 17:03:09 +1000

    Added login functionality to navbar and cleaned some stuff up

--- src/common/login_dropdown/login_dropdown.jsx
+++ src/common/login_dropdown/login_dropdown.jsx
@@ -1,33 +1,67 @@
 import { React, useState } from "react";
-import { FormControl, Form, Button, Dropdown, DropdownButton, Container } from "react-bootstrap";
+import { FormControl, Form, Button, Dropdown, DropdownButton, Container, Row } from "react-bootstrap";
 import "./login_dropdown.css";
+import axios from "../../api/axios";
 
 function LoginDropdown() {
-    const [auth, setAuth] = useState(false);
+    const [success, setSuccess] = useState(false);
     const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-    function handleLogin() {
-        setAuth(!auth);
+    const LOGIN_URL = "/login";
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                LOGIN_URL,
+                JSON.stringify( { user: { username, password }} ),
+                { headers: { "Content-Type": "application/json" } }
+            );
+            setSuccess(true);
+            console.log(response);
+        }
+        catch (error) {
+            if (!error?.response) {
+                setErrorMessage("No server response.");
+            }
+            else {
+                setErrorMessage("Invalid credentials.");
+            }
+        }
     }
 
-    if (auth === false) {
+    function handleLogout(e) {
+        e.preventDefault();
+        setSuccess(false);
+    }
+
+    if (success === false) {
         return (
             <DropdownButton title="Log In">
                 <Form className="d-flex">
-                    <FormControl
-                        type="text"
-                        placeholder="Username"
-                        aria-label="Username"
-                        onChange={(e) => {
-                            setUsername(e.target.value);
-                        }}
-                    />
-                    <FormControl
-                        type="password"
-                        placeholder="Password"
-                        aria-label="Password"
-                    />
-                    <Button type="submit" onClick={handleLogin}>Login</Button>
+                    <Container>
+                        <Row>
+                            <FormControl
+                                type="text"
+                                placeholder="Username"
+                                aria-label="Username"
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </Row>
+                        <Row>
+                            <FormControl
+                                type="password"
+                                placeholder="Password"
+                                aria-label="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Row>
+                        <Row>
+                            <Button type="submit" onClick={handleLogin}>Login</Button>
+                        </Row>
+                    </Container>
                 </Form>
             </DropdownButton>   
         )
@@ -36,7 +70,7 @@ function LoginDropdown() {
         return (
             <div>
                 Welcome, {username}!
-                <Button onClick={handleLogin}>Log Out</Button>
+                <Button onClick={handleLogout}>Log Out</Button>
             </div>
         )
     }
